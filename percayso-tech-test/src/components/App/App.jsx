6e4ba809b1bf497449cb6fcf5c4e199ad147b74e
GabrielFdeOliveira <gbl.diesel@hotmail.com>
2023-02-06 20:12:24 +0000

    Axios working properly, news being rendered

--- percayso-tech-test/src/components/App/App.jsx
+++ percayso-tech-test/src/components/App/App.jsx
@@ -1,19 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Card from '../Card/Card'
+import Search from '../Search/Search'
 import AppCSS from './App.module.css'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios.get(
+        'https://gnews.io/api/v3/search?q=technology&token=3b72fd7da7be1b12071d51d6ef17d853'
+      );
+      setArticles(result.data.articles);
+    }
+    fetchData();
+  }, []);
 
   return (
-    <div className="App">
-      
-      <div className={AppCSS.btn}>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>      
+    <div>
+      {articles.map((article) => (
+        <div key={article.title}>
+          <h3>{article.title}</h3>
+          <p>{article.description}</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
